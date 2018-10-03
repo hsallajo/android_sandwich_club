09e18e72de8f94f3b567ef727a7c531e902aa830
Salla Huoponen <salla.huoponen@gmail.com>
2018-10-02 17:11:45 -0700

    Converts fixed json key strings to constants

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -3,7 +3,6 @@ package com.udacity.sandwichclub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -12,6 +11,7 @@ import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
 import java.util.ListIterator;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
@@ -20,13 +20,20 @@ public class DetailActivity extends AppCompatActivity {
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
 
-    @BindView(R.id.image_sandwich) ImageView ingredientsIv;
-    @BindView((R.id.also_known_tv)) TextView alsoKnownTV;
-    @BindView(R.id.also_known_tv_label) TextView alsoKnownTVLabel;
-    @BindView(R.id.origin_tv) TextView placeOfOrigin;
-    @BindView(R.id.origin_tv_label) TextView placeOfOriginLabel;
-    @BindView(R.id.ingredients_tv) TextView incredientsTV;
-    @BindView(R.id.description_tv) TextView description;
+    @BindView(R.id.image_sandwich)
+    ImageView ingredientsIv;
+    @BindView((R.id.also_known_tv))
+    TextView alsoKnownTV;
+    @BindView(R.id.also_known_tv_label)
+    TextView alsoKnownTVLabel;
+    @BindView(R.id.origin_tv)
+    TextView placeOfOrigin;
+    @BindView(R.id.origin_tv_label)
+    TextView placeOfOriginLabel;
+    @BindView(R.id.ingredients_tv)
+    TextView ingredientsTV;
+    @BindView(R.id.description_tv)
+    TextView description;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -71,52 +78,57 @@ public class DetailActivity extends AppCompatActivity {
     }
 
     private void populateUI(Sandwich sandwich) {
-
         String alsoKnown = "";
-        if(!sandwich.getAlsoKnownAs().isEmpty() && sandwich.getAlsoKnownAs() != null){
+
+        if (!sandwich.getAlsoKnownAs().isEmpty()
+                && sandwich.getAlsoKnownAs() != null) {
+
             StringBuilder b = new StringBuilder();
             ListIterator<String> i = sandwich.getAlsoKnownAs().listIterator();
-            while(i.hasNext()){
+
+            while (i.hasNext()) {
+
                 b.append(i.next());
-                if(i.hasNext())
+                if (i.hasNext())
                     b.append(", ");
             }
 
             alsoKnown = b.toString();
         }
 
-        if(alsoKnown.equals("")){
+        if (alsoKnown.equals("")) {
             alsoKnownTV.setVisibility(View.GONE);
             alsoKnownTVLabel.setVisibility(View.GONE);
-        }
-        else {
+        } else {
             alsoKnownTV.setVisibility(View.VISIBLE);
             alsoKnownTV.setText(alsoKnown);
         }
 
-        if(sandwich.getPlaceOfOrigin().equals("")) {
+        if (sandwich.getPlaceOfOrigin().equals("")) {
             placeOfOrigin.setVisibility(View.GONE);
             placeOfOriginLabel.setVisibility(View.GONE);
-        }
-        else{
+        } else {
             placeOfOrigin.setVisibility(View.VISIBLE);
             placeOfOrigin.setText(sandwich.getPlaceOfOrigin());
         }
 
-        StringBuilder incredientsBuilder = new StringBuilder();
-        String incredients = "";
-        if(!sandwich.getIngredients().isEmpty() && sandwich.getIngredients() != null){
+        StringBuilder ingredientsBuilder = new StringBuilder();
+        String ingredients = "";
+
+        if (!sandwich.getIngredients().isEmpty()
+                && sandwich.getIngredients() != null) {
+
             ListIterator<String> i = sandwich.getIngredients().listIterator();
-            while(i.hasNext()){
-                incredientsBuilder.append(i.next());
-                if(i.hasNext())
-                    incredientsBuilder.append(", ");
+            while (i.hasNext()) {
+                ingredientsBuilder.append(i.next());
+                if (i.hasNext())
+                    ingredientsBuilder.append(", ");
             }
 
-            incredients = incredientsBuilder.toString();
+            ingredients = ingredientsBuilder.toString();
         }
 
-        incredientsTV.setText(incredients);
+        ingredientsTV.setText(ingredients);
 
         description.setText(sandwich.getDescription());
     }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -13,40 +13,47 @@ public class JsonUtils {
 
     private static final String TAG = JsonUtils.class.getSimpleName();
 
+    public static final String KEY_NAME = "name";
+    public static final String KEY_MAIN_NAME = "mainName";
+    public static final String KEY_ALSO_KNOWN_AS = "alsoKnownAs";
+    public static final String KEY_PLACE_OF_ORIGIN = "placeOfOrigin";
+    public static final String KEY_DESCRIPTION = "description";
+    public static final String KEY_IMAGE = "image";
+    public static final String KEY_INGREDIENTS = "ingredients";
+
     public static Sandwich parseSandwichJson(String json) {
 
         Sandwich s = null;
 
         try {
+
             JSONObject jsonObject = new JSONObject(json);
-            JSONObject nameObject = jsonObject.getJSONObject("name");
+            JSONObject nameObject = jsonObject.getJSONObject(KEY_NAME);
 
-            // main name
-            String mainName = nameObject.optString("mainName");
+            String mainName = nameObject.optString(KEY_MAIN_NAME);
 
-            // also known as
-            JSONArray alsoKnownAsArray = nameObject.optJSONArray("alsoKnownAs");
+            JSONArray alsoKnownAsArray = nameObject.optJSONArray(KEY_ALSO_KNOWN_AS);
             List<String> alsoKnownAs = new ArrayList<String>();
             int i=0;
-            while(alsoKnownAsArray != null && !alsoKnownAsArray.isNull(i)){
+
+            while(alsoKnownAsArray != null
+                    && !alsoKnownAsArray.isNull(i)){
                 alsoKnownAs.add(alsoKnownAsArray.optString(i));
                 i++;
             }
 
-            // place of origin
-            String placeOfOrigin = jsonObject.optString("placeOfOrigin");
+            String placeOfOrigin = jsonObject.optString(KEY_PLACE_OF_ORIGIN);
 
-            // description
-            String description = jsonObject.optString("description");
+            String description = jsonObject.optString(KEY_DESCRIPTION);
 
-            // image
-            String image = jsonObject.optString("image");
+            String image = jsonObject.optString(KEY_IMAGE);
 
-            // ingredients
-            JSONArray ingredientsArray = jsonObject.optJSONArray("ingredients");
+            JSONArray ingredientsArray = jsonObject.optJSONArray(KEY_INGREDIENTS);
             List<String> ingredients = new ArrayList<String>();
             i=0;
-            while(ingredientsArray != null && !ingredientsArray.isNull(i)){
+
+            while(ingredientsArray != null
+                    && !ingredientsArray.isNull(i)){
                 ingredients.add(ingredientsArray.optString(i));
                 i++;
             }
@@ -54,7 +61,7 @@ public class JsonUtils {
             s = new Sandwich(mainName, alsoKnownAs, placeOfOrigin, description, image, ingredients);
 
         } catch (JSONException e) {
-            Log.e(TAG, "Problem parsing the sandwhich JSON results", e);
+            Log.e(TAG, "Problem parsing JSON results", e);
         }
 
         return s;
